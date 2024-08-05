7af660d5c696a095f2e3ae502f7f8a5252a0e678
Ivan Savrikov <yeahsavrikov@gmail.com>
2024-08-05 23:17:56 +0700

    Added a KinopoiskApiService functions, added a RestSharp library, defined KinopoiskParser

--- Cinema/Services/KinopoiskApiService.cs
+++ Cinema/Services/KinopoiskApiService.cs
@@ -1,7 +1,56 @@
-﻿namespace Cinema.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using RestSharp;
+using Cinema.Models.Entities;
+
+namespace Cinema.Services
 {
     public class KinopoiskApiService
     {
+        private readonly KinopoiskParserService _parser;
+        private readonly RestClient _restClient;
+        private readonly string _baseUrl;
+        private readonly string _token;
 
+        public KinopoiskApiService(KinopoiskParserService parser)
+        {
+            _parser = parser;
+            _baseUrl = "https://kinopoiskapiunofficial.tech/";
+            _token = "7d3e436f-f59c-46dd-989d-dac71211f263";
+            _restClient = new RestClient(_baseUrl);
+            _restClient.AddDefaultHeader("X-API-KEY", _token);
+        }
+
+        public async Task<FilmEntity> GetFilmInfoByIdAsync(int id)
+        {
+            var request = new RestRequest($"api/v2.2/films/{id}", Method.Get);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+                throw new Exception("Error retrieving film: " + response.ErrorMessage);
+            FilmEntity film = _parser.ParseSingleFilm(response.Content);
+            return film;
+        }
+
+        // 1 >= page <= 5
+        public async Task<IEnumerable<FilmEntity>> GetFilmsOnPageAsync(int page)
+        {
+            var request = new RestRequest($"api/v2.2/films?page={page}", Method.Get);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+                throw new Exception("Error retrieving film: " + response.ErrorMessage);
+            var films = _parser.ParseFilmCollection(response.Content);
+            return films;
+        }
+
+        public async Task<IEnumerable<GenreEntity>> GetAllGenresAsync()
+        {
+            var request = new RestRequest($"api/v2.2/films/filters", Method.Get);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+                throw new Exception("Error retrieving film: " + response.ErrorMessage);
+            var genres = _parser.ParseGenres(response.Content);
+            return genres;
+        }
     }
 }
---
+++ Cinema/Services/KinopoiskParserService.cs
@@ -0,0 +1,23 @@
+﻿using Cinema.Models.Entities;
+using System.Collections.Generic;
+
+namespace Cinema.Services
+{
+    public class KinopoiskParserService
+    {
+        public FilmEntity ParseSingleFilm(string jsonString)
+        {
+            return new FilmEntity();
+        }
+
+        public List<FilmEntity> ParseFilmCollection(string jsonString)
+        {
+            return new List<FilmEntity>();
+        }
+
+        public List<GenreEntity> ParseGenres(string jsonString)
+        {
+            return new List<GenreEntity>();
+        }
+    }
+}
