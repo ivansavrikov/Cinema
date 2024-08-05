31bf5794acdbd06e35de184574ffbd4b9865a44c
Ivan Savrikov <yeahsavrikov@gmail.com>
2024-08-05 22:49:38 +0700

    Added Film and Genre entities

---
+++ Cinema/Models/Entities/FilmEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Cinema.Models.Entities
+{
+    public class FilmEntity
+    {
+        public int Id { get; set; }
+        public int KinoposikId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+        public string PosterUrl { get; set; }
+        public List<GenreEntity> Genres { get; set; } = new List<GenreEntity>();
+    }
+}
---
+++ Cinema/Models/Entities/GenreEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Cinema.Models.Entities
+{
+    public class GenreEntity
+    {
+        public int Id { get; set; }
+        public string KinoposikId { get; set; }
+        public string Title { get; set; }
+        public List<FilmEntity> Films { get; set; } = new List<FilmEntity>();
+    }
+}
